64d36d2d63f12208e5af2b07115d47aa5905e447
Seu Nome Novo <brunomitchell.ofc@gmail.com>
2026-03-23 11:07:57 +0100

    fix: switch to SQLite for Render deploy

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:32229256@127.0.0.1:3307/todoapplicationdatabase'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todoapplication.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
