1d19b9d87b5f78ed06b9a422ba7faf488c93d782
Seu Nome Novo <brunomitchell.ofc@gmail.com>
2026-03-23 16:26:37 +0100

    fix: cleanup main.py and absolute imports

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,26 +1,32 @@
 import os
 from fastapi import FastAPI, Request, status
-from .models import Base
-from .database import engine
-from .routers import auth, todos, admin, users
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 
+# Imports absolutos para evitar erros no Linux/Render
+from TodoApp.models import Base
+from TodoApp.database import engine
+from TodoApp.routers import auth, todos, admin, users
+
 app = FastAPI()
 
+# Criar as tabelas no banco de dados (SQLite/Postgres)
+Base.metadata.create_all(bind=engine)
+
+# Configuração da pasta Static (CSS/JS)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 static_path = os.path.join(current_dir, "static")
-
 app.mount("/static", StaticFiles(directory=static_path), name="static")
 
 @app.get("/")
-def test(request: Request):
-    return RedirectResponse(url="/todos/todo-page", status_code=status.HTTP_302_FOUND)
+def redirect_to_login(request: Request):
+    return RedirectResponse(url="/auth/login-page", status_code=status.HTTP_302_FOUND)
 
 @app.get("/healthy")
 def health_check():
     return {"status": "Healthy"}
 
+# Inclusão das rotas
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
